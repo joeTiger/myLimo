3ba827e55b79672d20afcb2bddb5e7270057dc3a
joeTiger <joetiger05@gmail.com>
2016-05-05 17:32:36 +0300

    modify comment

--- myLimo/Controllers/BaseController.cs
+++ myLimo/Controllers/BaseController.cs
@@ -17,7 +17,7 @@ namespace myLimo.Controllers
         protected override void Initialize(System.Web.Routing.RequestContext requestContext)
         {
 
-            //Initialize...
+            //Initialize...in baseController
             base.Initialize(requestContext);
         }
 
